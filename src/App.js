40969df1fdfe852e3d13c61148fd6abf1aa9bb09
luckkalpna <107942615+luckkalpna@users.noreply.github.com>
2022-10-06 17:53:45 +0530

    Correction

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import logo from './logo.svg';
-import './App.css';
+
 import Navbar from './components/Navbar'
 import News from './components/News'
 import React, { Component } from 'react';
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,9 +8,7 @@ export class Navbar extends Component {
 			<div>
 				<nav className="navbar navbar-expand-lg bg-light">
 					<div className="container-fluid">
-						<Link className="navbar-brand" to="/">
-							NewsApp 
-						</Link>
+						<Link className="navbar-brand" to="/">NewsApp </Link>
 						<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 							<span className="navbar-toggler-icon"></span>
 						</button>
