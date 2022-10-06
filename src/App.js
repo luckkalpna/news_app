dc07aaae93119c5947d2ac7f05e6450d844cc69e
luckkalpna <107942615+luckkalpna@users.noreply.github.com>
2022-10-06 15:53:42 +0530

    aa

--- src/App.js
+++ src/App.js
@@ -2,15 +2,46 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar'
 import News from './components/News'
-
 import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Navbar/> 
-        <News pageSize={6}/> 
+        <Router>
+          <Navbar/>
+            <Switch>
+              <Route path="/">
+                <News pageSize={6} country="in" category="general"/>
+              </Route>
+              <Route path="/business">
+                <News pageSize={6} country="in" category="business"/>
+              </Route>
+              <Route path="/entertainment">
+                <News pageSize={6} country="in" category="entertainment"/>
+              </Route>
+              <Route path="/general">
+                <News pageSize={6} country="in" category="general"/>
+              </Route>
+              <Route path="/health">
+                <News pageSize={6} country="in" category="health"/>
+              </Route>
+              <Route path="/science">
+                <News pageSize={6} country="in" category="science"/>
+              </Route>
+              <Route path="/sports">
+                <News pageSize={6} country="in" category="sports"/>
+              </Route>
+              <Route path="/technology">
+                <News pageSize={6} country="in" category="technology"/>
+              </Route>
+            </Switch>
+        </Router>
       </div>
     );
   }
